83f0af9845f76a64f7c522b109c0d9309ddded40
anwin112 <anwinkbiju.com>
2024-07-28 22:05:56 +0530

    new changes

--- q3.py
+++ q3.py
@@ -2,10 +2,13 @@
 
 
 letters=['A','B','C','D','E']
+n=5
 for i in range(0,5):
+    print(" " * (n-i), end=" ")
     for j in range(-1,i):
-        print(letters[j+1],end="")
+        print(letters[j+1],' ',end="")
     print()
+print()
 
 for i in range(1,6):
     for j in range(0,i):
--- q5.py
+++ q5.py
@@ -4,10 +4,12 @@
 #(b) range(1,100)
 
 #a
+print("range(1,50)")
 for i in range(1,50):
     if(i%2==0):
         print(i,"square is ",i*i)
 #b
+print("range(1,100)")
 for i in range(1,100):
     if(i%2==0):
         print(i,"square is ",i*i)
